b51f5939ddb94bd19e6c96e24f8f3b14a5ee3b85
Eternlyours <Nazmetdinov2001@yandex.ru>
2025-05-23 12:06:10 +0500

    first

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function sum() {
+    let a, b, sum;
+    a = document.getElementById("a").value;
+    alert(a)
+    b = document.getElementById("b").value;
+    sum = a + b ;
+    
+    alert(sum)
+}
