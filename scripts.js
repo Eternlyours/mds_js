034ae32e591a9c305217a4427a2d4c958bcabfd6
Eternlyours <Nazmetdinov2001@yandex.ru>
2025-05-23 15:15:47 +0500

    tasks modified

--- scripts.js
+++ scripts.js
@@ -27,12 +27,64 @@ function sum() {
 //     console.log(a, b)
 // }
 
+// {
+//     let number, a1, a2, a3, a4;
+//     number = parseInt(prompt("4-ое число"));
+//     a1 = Math.floor(number % 10);
+//     a2 = Math.floor(number / 10 % 10);
+//     a3 = Math.floor(number / 100 % 10);
+//     a4 = Math.floor(number / 1000 % 10);
+//     console.log(a1, a2, a3, a4);
+// }
+
+// найти максимум из двух чисел
+
+// {
+//     let x, y, z, max;
+//     x = parseFloat(prompt('Первое число'));
+//     y = parseFloat(prompt('Введите второе число'));
+//     z = parseFloat(prompt('Введите третье число'));
+
+//     max = x;
+
+//     if (x >= y && x >= z ) {
+//         console.log(x + ' больше ' + y + ' и ' + z)
+//     }
+//     else if (y >= x && y >= z) {
+//         console.log(y + ' больше ' + z + ' и ' + x)
+//     }
+//     else {
+//         console.log(z + ' больше ' + y + ' и ' + x)
+//     }
+// }
+
+// Даны n-чисел, найти максимум среди них
+
+// {
+//     let n, a, max;
+//     n = parseInt(prompt('Введите количество чисел'))
+//     max = parseInt(prompt('Введите число'))
+//     for (let i = 1; i < n; i++) {
+//         console.log(i)
+//         a = parseInt(prompt('Введите число'))
+//         if (a > max) {
+//             max = a
+//         }
+//     }
+//     alert(max)
+// }
+
+// Дана последовательность чисел которая заканчивается вводом числа 0, найти максимум среди этих чисел
+
 {
-    let number, a1, a2, a3, a4;
-    number = parseInt(prompt("4-ое число"));
-    a1 = Math.floor(number % 10);
-    a2 = Math.floor(number / 10 % 10);
-    a3 = Math.floor(number / 100 % 10);
-    a4 = Math.floor(number / 1000 % 10);
-    console.log(a1, a2, a3, a4);
+    let a, max;
+    a = parseInt(prompt('Введите число'))
+    max = a
+    while ( a != 0 ) {
+        a = parseInt(prompt('Введите число'))
+        if (a > max) {
+            max = a
+        }
+    }
+    alert(max)
 }
--- task1.js
+++ task1.js
@@ -15,14 +15,43 @@
 
 // 4.10. Даны радиус круга и сторона квадрата. У какой фигуры площадь больше? 
 
-{
-    let p1, r;
-    const P = 3.14;
-    p1 = parseFloat(prompt('Введите сторону квадрата'));
-    r = parseFloat(prompt('Введите радиус круга'));
-    Scircle = Math.pow(r, 2) * P;
-    Ssquare = Math.pow(p1, 2)
-    console.log(Scircle, Ssquare)
+// {
+//     let p1, r;
+//     const P = 3.14;
+//     p1 = parseFloat(prompt('Введите сторону квадрата'));
+//     r = parseFloat(prompt('Введите радиус круга'));
+//     Scircle = Math.pow(r, 2) * P;
+//     Ssquare = Math.pow(p1, 2)
+//     console.log(Scircle, Ssquare)
 
-    
+//     if (Scircle > Ssquare)
+//     {
+//         console.log('Площадь круга больше ' + Scircle)
+//     }
+//     if (Scircle < Ssquare){
+//         console.log('Площадь квадрата больше ' + Ssquare)
+//     }
+//     if (Scircle == Ssquare){
+//         console.log('Площади равны ' + Scircle + ' == ' + Ssquare)
+//     }
+// }
+
+// 4.30. Дано трехзначное число. Определить: б) является ли произведение его цифр трехзначным числом; 
+
+{
+    let digit, length, secondDigit;
+    digit = parseInt(prompt('Введите 3-ое число'))
+    first = Math.floor(digit / 100 % 100)
+    second = Math.floor(digit / 10 % 10)
+    third = digit % 10
+    secondDigit = first * second * third
+    length = String(secondDigit).length
+    if (length === 3 ) {
+        console.log('Число трехзначное ' + length)
+    }
+    else {
+        console.log('Число не трёхзначное ' + length)
+    }
+
+
 }
